5664c1ea6ee3162fe3fdb688dfc4a4e4f52f0026
techiejd <62455039+techiejd@users.noreply.github.com>
2023-03-23 22:48:38 -0500

    Adds a little more structure to UI

--- ui/modules/admin.page.tsx
+++ ui/modules/admin.page.tsx
@@ -1,7 +1,69 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import PosiPage from "./posiPage";
 import { useAccounts } from "./useAccounts";
+import { useState } from "react";
+import { PublicKey } from "snarkyjs";
+import { useBlockchainState } from "./blockchainContext";
 
 export default function Admin() {
   const accounts = useAccounts();
-  return <PosiPage type={"admin"} accounts={accounts} />;
+  const bState = useBlockchainState();
+  const [posiContract, setPosiContract] = useState<PublicKey | undefined>();
+  const [weRaceContracts, setWeRaceContracts] = useState<PublicKey[]>([]);
+
+  const deployPosi = () => {
+    // TODO(techiejd): Change to actual deploy.
+    setPosiContract(bState?.local.testAccounts[0].publicKey);
+  };
+
+  const deployWeRace = () => {
+    // TODO(techiejd): Change to actual deploy.
+    setWeRaceContracts((weRaceContracts) => {
+      return [...weRaceContracts, bState?.local.testAccounts[0].publicKey];
+    });
+  };
+
+  return (
+    <PosiPage type={"admin"} accounts={accounts}>
+      <Stack spacing={2} margin={2}>
+        {posiContract == undefined ? (
+          <Button onClick={deployPosi}>Deploy PoSI NFT Contract</Button>
+        ) : (
+          <Typography border={1}>
+            PoSI NFT Contract: {posiContract.toBase58()}
+          </Typography>
+        )}
+        <Button onClick={deployWeRace}>Deploy WeRace Contract</Button>
+        {weRaceContracts.map((weRaceContract, i) => {
+          return (
+            <Card key={i}>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <CardContent>
+                  This WeRace's addy: {weRaceContract.toBase58()}
+                  <TextField label="Winner?" required />
+                </CardContent>
+                <CardActions>
+                  <Button size="small" type="submit" disabled>
+                    Submit Winner
+                  </Button>
+                </CardActions>
+              </form>
+            </Card>
+          );
+        })}
+      </Stack>
+    </PosiPage>
+  );
 }
--- ui/modules/maker.page.tsx
+++ ui/modules/maker.page.tsx
@@ -1,7 +1,35 @@
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Stack,
+} from "@mui/material";
 import PosiPage from "./posiPage";
 import { useAccounts } from "./useAccounts";
 
 export default function Maker() {
   const accounts = useAccounts();
-  return <PosiPage type={"maker"} accounts={accounts} />;
+  return (
+    <PosiPage type={"maker"} accounts={accounts}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault;
+        }}
+      >
+        <Stack>
+          <FormControl>
+            <FormLabel>
+              Ayo! Please upload your "Proof of Social Impact"!
+            </FormLabel>
+            <Input type="file" required />
+          </FormControl>
+          <Button type="submit" variant="contained">
+            Upload
+          </Button>
+        </Stack>
+      </form>
+    </PosiPage>
+  );
 }
